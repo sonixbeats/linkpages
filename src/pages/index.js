3c495f70dadd9a09c5a91b746e1eb604e67202ca
SoNiX <82837359+sonixbeats@users.noreply.github.com>
2024-02-09 17:57:03 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="text-lg font-medium">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className="flex space-x-4 bg-white rounded-lg p-10">
+        <Image
+          src="/images/nextjs.svg"
+          alt="Next.js Logo"
+          width={100}
+          height={100}
+        />
+        <h1 className="text-black break-words">
+          Welcome to my page links !
+          </h1>
+          
+      </div>
     </div>
   );
 }
